d7f0bad4a58e2dfd280a26d14b3d9a1f572d4ab8
Emmanuel Vazquez <emmanuel.vazquez06@gmail.com>
2022-05-03 10:27:38 +0200

    fix
    fix bug where player was able to enter in non-integer characters as a guess

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -1,8 +1,3 @@
-/*
-In this file, you will also include the event listeners that are needed to interact with your HTML file when
-a user clicks a button or adds a guess to the input field.
-*/
-
 function generateWinningNumber() {
   // the math.random returns number between 0 and 1 with decimals, but not including 1. so if we do * 10, we will get number between 0 and 10(not including 10), but * 100 will give us a number up to 0-99 not including 100. it can be a two digit number. so in order to include 10 or 100, we add the + 1 at the end, for example if we returned a number that was .999, we would be able to turn it into 99.9 and the plus 1 will let us reach 100, so itd be 100.9, then we'd round down, and itd be 100.
   return Math.floor(Math.random() * 100) + 1;
@@ -43,25 +38,25 @@ class Game {
     return this.playersGuess < this.winningNumber;
   }
   playersGuessSubmission(num) {
-    if (typeof num !== "number" || num < 1 || num > 100) {
-      throw "That is an invalid guess.";
+    if (num < 1 || num > 100) {
+      throw 'That is an invalid guess.';
     }
     this.playersGuess = num;
     return this.checkGuess();
   }
 
   checkGuess() {
-    let feedbackText = "";
+    let feedbackText = '';
     if (this.playersGuess === this.winningNumber) {
-      feedbackText = "You Win!";
+      feedbackText = 'You Win!';
       this.gameOver = true;
     } else if (this.pastGuesses.includes(this.playersGuess))
-      feedbackText = "You have already guessed that number.";
+      feedbackText = 'You have already guessed that number.';
     else {
       this.pastGuesses.push(this.playersGuess);
 
       if (this.pastGuesses.length === 5) {
-        feedbackText = "You Lose!";
+        feedbackText = 'You Lose!';
         this.gameOver = true;
       } else {
         let diff = this.difference();
@@ -73,13 +68,13 @@ class Game {
         if (this.pastGuesses.length >= 3) {
           // const hintButton = document.getElementById("hint");
           // hintButton.className += " hidden";
-          let hintButton = document.getElementById("hint");
-          hintButton.classList.remove("hidden"); //this uses a method on the element object that makes it easier to add and remove classes from an element
+          let hintButton = document.getElementById('hint');
+          hintButton.classList.remove('hidden'); //this uses a method on the element object that makes it easier to add and remove classes from an element
         }
       }
     }
 
-    document.querySelector("#subtitle").innerHTML = feedbackText;
+    document.querySelector('#subtitle').innerHTML = feedbackText;
     document.querySelector(
       `#guessList li:nth-child(${this.pastGuesses.length})`
     ).innerHTML =
@@ -111,38 +106,40 @@ function newGame() {
 
 function playGame() {
   let game = newGame();
-  const submitGuessButton = document.getElementById("submit");
+  const submitGuessButton = document.getElementById('submit');
 
-  submitGuessButton.addEventListener("click", function () {
+  submitGuessButton.addEventListener('click', function () {
     //value is always in string form so we need to conver it to a number
-    const playersGuess = Number(document.querySelector("input").value);
+    const playersGuess = Number(document.querySelector('input').value);
 
-    document.querySelector("input").value = "";
+    document.querySelector('input').value = '';
 
-    if (!game.gameOver) game.playersGuessSubmission(playersGuess);
-  });
-
-  const playAgainButton = document.getElementById("reset");
-  playAgainButton.addEventListener("click", function () {
-    game = newGame();
-
-    let hintButton = document.getElementById("hint");
-    hintButton.classList.add("hidden");
-    document.querySelector("#subtitle").innerHTML =
-      "Pick a number from 0 - 100";
-    const listItems = document.getElementsByTagName("li");
-    for (listItem of listItems) {
-      listItem.innerHTML = "-";
+    if (playersGuess && !game.gameOver) {
+      game.playersGuessSubmission(playersGuess);
     }
   });
 
-  const hintButton = document.getElementById("hint");
-  hintButton.addEventListener("click", () => {
+  const playAgainButton = document.getElementById('reset');
+  playAgainButton.addEventListener('click', function () {
+    game = newGame();
+
+    let hintButton = document.getElementById('hint');
+    hintButton.classList.add('hidden');
+    document.querySelector('#subtitle').innerHTML =
+      'Pick a number from 0 - 100';
+    const listItems = document.getElementsByTagName('li');
+    for (listItem of listItems) {
+      listItem.innerHTML = '-';
+    }
+  });
+
+  const hintButton = document.getElementById('hint');
+  hintButton.addEventListener('click', () => {
     if (!game.gameOver) {
       const hintArr = game.provideHint();
       document.querySelector(
-        "#subtitle"
-      ).innerHTML = `Maybe one of these? ${hintArr.join(", ")}`;
+        '#subtitle'
+      ).innerHTML = `Maybe one of these? ${hintArr.join(', ')}`;
     }
   });
 }
